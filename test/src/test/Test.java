bb12c2aee1ddfbd81224b0a6a70a3b4b5197c7cd
Liyadi <18842475178@163.com>
2018-01-03 18:11:31 +0800

    测试

--- test/src/test/Test.java
+++ test/src/test/Test.java
@@ -7,8 +7,8 @@ public class Test {
 		double[] a={2.5,3.9,4.3,1.1};
 		double max=Arrays.stream(a).max().getAsDouble();
 		System.out.println(max);
-		System.out.println("改变1");
-		System.out.println("改变2");
-		System.out.println("改变3");
+		System.out.println("鏇存敼1");
+		System.out.println("鏇存敼2");
+		System.out.println("鏇存敼3");
 	}
 }
